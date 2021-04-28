3698a7b63bf678068c88320ff062dcc2f58c2e9e
Gonzalo Leyton <gonzalo.leyton@cencosud.cl>
2021-04-28 21:58:29 +0000

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -17,9 +17,9 @@ async function bootstrap() {
   const port = app.get(ConfigService).get('app.port') || process.env.APP_SERVER_PORT;
   const nodeEnv = app.get(ConfigService).get('app.node_env') || process.env.NODE_ENV;
   const apiVersion = app.get(ConfigService).get('app.version') || process.env.APP_VERSION;
-  const appName = app.get(ConfigService).get('app.name') || 'Purchase Service';
+  const appName = app.get(ConfigService).get('app.name') || 'Sync Service';
   const corsAllowed = app.get(ConfigService).get('app.cors_allowed') || process.env.CORS_ALLOWED_DOMAIN;
-  const basePath = nodeEnv !== 'development' ? 'ecom/purchase/api/' : 'api/';
+  const basePath = nodeEnv !== 'development' ? 'ecom/sync/api/' : 'api/';
   app.use(bodyParser.json({ limit: '50mb' }));
   generateTypeormConfigFile(config);
 
