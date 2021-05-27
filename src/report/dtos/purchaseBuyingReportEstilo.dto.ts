5f59b01c5c0b621cf5fe89e01863de9cf822cea4
Yendry Machado <yendry.machado@externos-cl.cencosud.com>
2021-05-27 11:34:40 -0400

    Validate null exception

--- src/report/dtos/purchaseBuyingReportEstilo.dto.ts
+++ src/report/dtos/purchaseBuyingReportEstilo.dto.ts
@@ -93,108 +93,109 @@ export class PurchaseBuyingReportEstilo extends PurchaseBuyingReport {
         _.flatten(purchaseStyles.map(purchaseStyle => {
             return purchaseStyle.colors.map(color => {
                 const styleData = stylesData.find(s => s.id === purchaseStyle.styleId);
-                const styleDetails = purchaseStyle.details[0];
-                const colorData = styleData.colors.find(c => c.id === color.styleColorId);
-                const sku = styleSkus.find(sku => sku.styleId === styleData.id);
-                const skuColor = sku?.skuColor.find(skuColor => skuColor.styleColorId === colorData.id);
-                const referentialSku = skuColor?.skuColorSize.find(skuColorSize => skuColorSize.sku);
-                if (styleData && styleDetails && colorData) {
-                    let brandManager = 'NO APLICA';
-                    if (styleDetails.brandManager && styleDetails.brandManager !== '-1') {
-                        const brandManagerUser = users?.find(u => u.id === parseInt(styleDetails.brandManager, null) ?? -1);
-                        brandManager = brandManagerUser ? `${brandManagerUser.firstName} ${brandManagerUser.lastName}` : styleDetails.brandManager;
+                const styleDetails = purchaseStyle.details[0];                
+                if (styleData && styleDetails) {
+                    const colorData = styleData.colors.find(c => c.id === color.styleColorId);                    
+                    if (colorData) {
+                        const sku = styleSkus.find(sku => sku.styleId === styleData.id);
+                        const skuColor = sku?.skuColor.find(skuColor => skuColor.styleColorId === colorData.id);
+                        const referentialSku = skuColor?.skuColorSize.find(skuColorSize => skuColorSize.sku);
+                        
+                        let brandManager = 'NO APLICA';
+                        if (styleDetails.brandManager && styleDetails.brandManager !== '-1') {
+                            const brandManagerUser = users?.find(u => u.id === parseInt(styleDetails.brandManager, null) ?? -1);
+                            brandManager = brandManagerUser ? `${brandManagerUser.firstName} ${brandManagerUser.lastName}` : styleDetails.brandManager;
+                        }
+                        let productManager = 'NO APLICA';
+                        if (styleDetails.productManager && styleDetails.productManager !== '-1') {
+                            const productManagerUser = users?.find(u => u.id === parseInt(styleDetails.productManager, null) ?? -1);
+                            productManager = productManagerUser ? `${productManagerUser.firstName} ${productManagerUser.lastName}` : styleDetails.productManager;
+                        }
+                        let designer = 'NO APLICA';
+                        if (styleDetails.designer && styleDetails.designer !== '-1') {
+                            const designerUser = users?.find(u => u.id === parseInt(styleDetails.designer, null) ?? -1);
+                            designer = designerUser ? `${designerUser.firstName} ${designerUser.lastName}` : styleDetails.designer;
+                        }
+                        const firsDeliveryDate = color.shippings.map(s => s.date).sort((a, b) => a.getTime() - b.getTime())[0];
+                        for (const shipping of color.shippings) {
+                            const cbm = parseFloat(styleData.cbm).toFixed(4);
+                            this.dataToExport.push({
+                                status: color.status.name,
+                                season: purchaseStyle.purchaseStore.purchase.seasonCommercial.name,
+                                productSeason: styleData.seasonProduct,
+                                tripDate: moment(purchaseStyle.purchaseStore.purchase.tripDate).format('MMM-yyyy'),
+                                unit: purchaseStyle.purchaseStore.store.name,
+                                division: styleData.division,
+                                departmentCode: styleData.departmentCode,
+                                department: styleData.department,
+                                classTypeCode: styleData.classTypeCode,
+                                classType: styleData.classType,
+                                providerCode: styleDetails.provider.codeJda,
+                                provider: styleDetails.provider.name,
+                                cso: styleDetails.cso.name,
+                                brand: styleData.brand,
+                                styleCode: styleData.code,
+                                sku: referentialSku?.sku || '',
+                                atcId: (purchaseStyle.purchaseStore.store.shortName !== 'PW' &&
+                                        purchaseStyle.purchaseStore.store.shortName !== 'TP') ? referentialSku?.atc || '' : '',
+                                color: colorData.colorName,
+                                colorCode: colorData.colorCode,
+                                colorShortName: colorData.colorShortName,
+                                collection: styleDetails.collection,
+                                profile: styleData.profileJdaCode,
+                                origin: styleDetails.origin.name,
+                                shippingMethod: styleDetails.shippingMethod.name,
+                                price: styleDetails.price,
+                                sato: styleDetails.sato,
+                                ratio: styleDetails.ratio.ratio,
+                                size: styleDetails.size.size,
+                                packingMethod: styleDetails.packingMethod.name,
+                                hanger: styleDetails.hanger ? 'YES' : 'NO',
+                                vstTag: styleDetails.vstTag ? 'YES' : 'NO',
+                                atc: styleDetails.atc ? 'YES' : 'NO',
+                                unitsPerInner: styleDetails.ratio.ratio.split('-').map(x => parseInt(x, null)).reduce((a, b) => a + b), // calculate by ratio,
+                                innerPerMaster: styleData.divisionMaster, // calculate by ratio,
+                                cbm: Number(cbm),
+                                totalCbm: styleData.cbm * shipping.units,
+                                dimension: styleData.dimension,
+                                rse: styleDetails.rse?.name || '',
+                                sustainableFeature: styleDetails.sustainableFeature?.name || '',
+                                composition: styleDetails.composition || '',
+                                weaving: styleDetails.fabricWeaving || '',
+                                construction: styleDetails.fabricConstruction || '',
+                                weight: styleDetails.fabricWight || '',
+                                gauge: styleDetails.gauge || '',
+                                referencialProvider: styleDetails.referencialProvider || '',
+                                // totalQty: color.getTotalUnits(),
+                                firstDelivery: firsDeliveryDate ? moment(firsDeliveryDate).format('DD-MMM-yyyy') : '', // check
+                                fob: styleDetails.fob *(1/1),
+                                totalFob: shipping.units * styleDetails.fob,
+                                dollarBought: styleDetails.dollarChange*(1/1) || 0,
+                                importFactor: styleDetails.importFactor * 1 || 0,
+                                cost: (styleDetails.fob * styleDetails.dollarChange * styleDetails.importFactor) || 0,
+                                totalCost: ((styleDetails.fob * styleDetails.dollarChange * styleDetails.importFactor) * shipping.units * 1.0) || 0, // TODO: Pending
+                                totalRetail: (styleDetails.price * shipping.units) * 1.0, // TODO: Pending
+                                brandManager,
+                                productManager,
+                                designer,
+                                piNumber: shipping.piName,
+                                country: purchaseStyle.purchaseStore.store.destinyCountry.name,
+                                sticker: styleDetails.seasonSticker.name,
+                                internetDescription: styleDetails.internetDescription,
+                                segment: styleDetails.segment?.name || '',
+                                delivery: shipping.shipping,
+                                units: shipping.units,
+                                date: moment(shipping.date).format('DD-MMM-yyyy'),
+                                category: styleDetails.category.name,
+                                exitPort: styleDetails.exitPort.name,
+                                exitPortCode: styleDetails.exitPort.jdaCode,
+                                approvalDate: purchaseStyle?.purchaseStore?.purchase?.approvalDate ? moment(purchaseStyle.purchaseStore.purchase.approvalDate).format('DD-MMM-yyyy') : '',
+                                ocJda: shipping['oc'].map(oc => oc.ponumb).join('/'),
+                                impNum: shipping['oc'][0] ? `${purchaseStyle.purchaseStore.store.impnumpfx}${shipping['oc'][0].poedat.toString().substring(0, 4)}${shipping['oc'][0].ponumb}` : null,
+                            });
+                        }
                     }
-                    let productManager = 'NO APLICA';
-                    if (styleDetails.productManager && styleDetails.productManager !== '-1') {
-                        const productManagerUser = users?.find(u => u.id === parseInt(styleDetails.productManager, null) ?? -1);
-                        productManager = productManagerUser ? `${productManagerUser.firstName} ${productManagerUser.lastName}` : styleDetails.productManager;
-                    }
-                    let designer = 'NO APLICA';
-                    if (styleDetails.designer && styleDetails.designer !== '-1') {
-                        const designerUser = users?.find(u => u.id === parseInt(styleDetails.designer, null) ?? -1);
-                        designer = designerUser ? `${designerUser.firstName} ${designerUser.lastName}` : styleDetails.designer;
-                    }
-                    const firsDeliveryDate = color.shippings.map(s => s.date).sort((a, b) => a.getTime() - b.getTime())[0];
-                    for (const shipping of color.shippings) {
-                        const cbm = parseFloat(styleData.cbm).toFixed(4);
-                        this.dataToExport.push({
-                            status: color.status.name,
-                            season: purchaseStyle.purchaseStore.purchase.seasonCommercial.name,
-                            productSeason: styleData.seasonProduct,
-                            tripDate: moment(purchaseStyle.purchaseStore.purchase.tripDate).format('MMM-yyyy'),
-                            unit: purchaseStyle.purchaseStore.store.name,
-                            division: styleData.division,
-                            departmentCode: styleData.departmentCode,
-                            department: styleData.department,
-                            classTypeCode: styleData.classTypeCode,
-                            classType: styleData.classType,
-                            providerCode: styleDetails.provider.codeJda,
-                            provider: styleDetails.provider.name,
-                            cso: styleDetails.cso.name,
-                            brand: styleData.brand,
-                            styleCode: styleData.code,
-                            sku: referentialSku?.sku || '',
-                            atcId: (purchaseStyle.purchaseStore.store.shortName !== 'PW' &&
-                                    purchaseStyle.purchaseStore.store.shortName !== 'TP') ? referentialSku?.atc || '' : '',
-                            color: colorData.colorName,
-                            colorCode: colorData.colorCode,
-                            colorShortName: colorData.colorShortName,
-                            collection: styleDetails.collection,
-                            profile: styleData.profileJdaCode,
-                            origin: styleDetails.origin.name,
-                            shippingMethod: styleDetails.shippingMethod.name,
-                            price: styleDetails.price,
-                            sato: styleDetails.sato,
-                            ratio: styleDetails.ratio.ratio,
-                            size: styleDetails.size.size,
-                            packingMethod: styleDetails.packingMethod.name,
-                            hanger: styleDetails.hanger ? 'YES' : 'NO',
-                            vstTag: styleDetails.vstTag ? 'YES' : 'NO',
-                            atc: styleDetails.atc ? 'YES' : 'NO',
-                            unitsPerInner: styleDetails.ratio.ratio.split('-').map(x => parseInt(x, null)).reduce((a, b) => a + b), // calculate by ratio,
-                            innerPerMaster: styleData.divisionMaster, // calculate by ratio,
-                            cbm: Number(cbm),
-                            totalCbm: styleData.cbm * shipping.units,
-                            dimension: styleData.dimension,
-                            rse: styleDetails.rse?.name || '',
-                            sustainableFeature: styleDetails.sustainableFeature?.name || '',
-                            composition: styleDetails.composition || '',
-                            weaving: styleDetails.fabricWeaving || '',
-                            construction: styleDetails.fabricConstruction || '',
-                            weight: styleDetails.fabricWight || '',
-                            gauge: styleDetails.gauge || '',
-                            referencialProvider: styleDetails.referencialProvider || '',
-                            // totalQty: color.getTotalUnits(),
-                            firstDelivery: firsDeliveryDate ? moment(firsDeliveryDate).format('DD-MMM-yyyy') : '', // check
-                            fob: styleDetails.fob *(1/1),
-                            totalFob: shipping.units * styleDetails.fob,
-                            dollarBought: styleDetails.dollarChange*(1/1) || 0,
-                            importFactor: styleDetails.importFactor * 1 || 0,
-                            cost: (styleDetails.fob * styleDetails.dollarChange * styleDetails.importFactor) || 0,
-                            totalCost: ((styleDetails.fob * styleDetails.dollarChange * styleDetails.importFactor) * shipping.units * 1.0) || 0, // TODO: Pending
-                            totalRetail: (styleDetails.price * shipping.units) * 1.0, // TODO: Pending
-                            brandManager,
-                            productManager,
-                            designer,
-                            piNumber: shipping.piName,
-                            country: purchaseStyle.purchaseStore.store.destinyCountry.name,
-                            sticker: styleDetails.seasonSticker.name,
-                            internetDescription: styleDetails.internetDescription,
-                            segment: styleDetails.segment?.name || '',
-                            delivery: shipping.shipping,
-                            units: shipping.units,
-                            date: moment(shipping.date).format('DD-MMM-yyyy'),
-                            category: styleDetails.category.name,
-                            exitPort: styleDetails.exitPort.name,
-                            exitPortCode: styleDetails.exitPort.jdaCode,
-                            approvalDate: purchaseStyle?.purchaseStore?.purchase?.approvalDate ? moment(purchaseStyle.purchaseStore.purchase.approvalDate).format('DD-MMM-yyyy') : '',
-                            ocJda: shipping['oc'].map(oc => oc.ponumb).join('/'),
-                            impNum: shipping['oc'][0] ? `${purchaseStyle.purchaseStore.store.impnumpfx}${shipping['oc'][0].poedat.toString().substring(0, 4)}${shipping['oc'][0].ponumb}` : null,
-                        });
-                    }
-
                 }
-
             });
         }));
     }
